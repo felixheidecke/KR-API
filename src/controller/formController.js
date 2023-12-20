aede67246812429180b2c21aa3ad577a458f535e
Felix Heidecke <felix@heidecke.me>
2023-12-20 18:29:26 +0100

    Add form submitter as replyTo email

--- src/controller/formController.js
+++ src/controller/formController.js
@@ -84,6 +84,7 @@ export default async (App) => {
           from,
           bcc,
           to: emailAddresses.join(','),
+          replyTo: body.email?.trim() || undefined,
           subject: body.subject.trim(),
           text: jsonToText(content),
           attachments:
