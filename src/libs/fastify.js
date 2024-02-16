5140c5a1fc0f2982129f4fcb60e0123657976f8f
Felix Heidecke <felix@heidecke.me>
2024-02-16 14:44:04 +0100

    Update dependencies, improve logging

--- src/libs/fastify.js
+++ src/libs/fastify.js
@@ -1,33 +1,43 @@
 import Fastify from 'fastify'
 import fs from 'fs'
 import pkg from '../../package.json' assert { type: 'json' }
-import { cert, key } from '#config/app.config'
+import format from 'date-fns/format'
+
+const cert = process.env.APP_CERT || null
+const key = process.env.APP_KEY || null
 
 export default Fastify({
   logger: {
+    level: 'info',
     mixin() {
-      return { version: pkg.version }
+      return {
+        version: pkg.version
+      }
     },
+    redact: {
+      paths: ['pid', 'hostname'],
+      remove: true
+    },
+    timestamp: () =>
+      `,"timestamp":"${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}"`,
     serializers: {
       req(request) {
         return {
           method: request.method,
-          url: request.url,
-          origin: request.headers.origin
-            ? new URL(request.headers.origin).hostname
-            : 'unknown',
-          remote: request.ip
+          origin: request.headers.origin || 'unknown',
+          url: request.url
         }
       }
     }
   },
   trustProxy: true,
-  https: !!cert
-    ? {
-        cert: fs.readFileSync(cert),
-        key: fs.readFileSync(key),
-        http2: true,
-        allowHTTP1: true
-      }
-    : false
+  http2: true,
+  https:
+    !!cert && !!key
+      ? {
+          cert: fs.readFileSync(cert),
+          key: fs.readFileSync(key),
+          allowHTTP1: true
+        }
+      : false
 })
